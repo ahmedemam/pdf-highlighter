cf69779fda147cfa97180895a0b4969f810bc17f
ahmedemam <zizoahmed980@gail.com>
2019-09-22 14:43:15 +0200

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,123 @@
+import {Component, ElementRef} from '@angular/core';
+// import * as TextHighlighter from "texthighlighterjs";
+import {detectEncoding, detectFileEncoding} from 'char-encoding-detector';
+
+declare const TextHighlighter: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'pdf-highlighter-texthighlighter';
+  pdfSrc = './assets/compressed.tracemonkey-pldi-09.pdf';
+  serialized: string;
+  private textHighlighter = new TextHighlighter(document.body);
+  // highlightsObjectArray: [];
+  // highlightedObject: IHighlightedObject;
+  // pdfHighlightedObjects: IPdfHighlightedObject;
+  // user: IUser;
+  constructor(private elr: ElementRef) {
+    // const textHighlighter = new TextHighlighter(document.body);
+    this.textHighlighter.setColor('green');
+    // this.user = {
+    //   id: 1,
+    //   name: 'Ahmed Abd Elaziz',
+    //   profession: 'Software Engineer',
+    //   image: 'image/profile.png'
+    // };
+    // this.pdfHighlightedObjects.user = this.user;
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit(): void {
+    const values = JSON.parse(localStorage.getItem('a'));
+    console.log(values);
+    this.textHighlighter.normalizeHighlights(values);
+    // for (const element of values){
+    //   console.log(element.);
+    // }
+  }
+
+  onClearHighlight() {
+    this.textHighlighter.removeHighlights();
+  }
+
+  onSerializeData(){
+    this.serialized = this.textHighlighter.serializeHighlights();
+    console.log(this.serialized);
+    this.textHighlighter.removeHighlights();
+  }
+
+  onDeSerializedData(){
+    this.textHighlighter.removeHighlights();
+    this.textHighlighter.deserializeHighlights(this.serialized);
+  }
+
+  onFileSelected() {
+    const $file: any = document.querySelector('#file');
+    if (typeof (FileReader) !== undefined) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+      reader.readAsArrayBuffer($file.files[0]);
+    }
+  }
+
+  onRequestObject() {
+    const values = this.textHighlighter.getHighlights();
+    console.log(values);
+    // const dataObject = document.getElementsByClassName('highlighted');
+    // // console.log(dataObject);
+    // const highlightedObjects = [];
+    //
+    // for (let i = 0; i < dataObject.length ; i++) {
+    //   const highlightObject = {
+    //     index: i,
+    //     content: dataObject[i].textContent
+    //   };
+    //   highlightedObjects.push(highlightObject);
+    // }
+    // // this.highlightsObjectArray = highlightedObjects;
+    // console.log(highlightedObjects);
+    localStorage.setItem('a', JSON.stringify(values));
+    // console.log(this.textHighlighter.getHighlights());
+    // console.log(this.textHighlighter.serializeHighlights());
+  }
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  // onMakeHighlights(){
+  //   console.log(this.highlightsObjectArray);
+  //   const highlightedElementArray = this.highlightsObjectArray;
+  //   for (let element of highlightedElementArray){
+  //     this.textHighlighter.find(element.textContent);
+  // }
+  // }
+}
+
+// interface IPdfHighlightedObject {
+//   PdfName: string;
+//   user: IUser;
+//   highlightedObjects: [IHighlightedObject];
+// }
+//
+// interface IHighlightedObject {
+//   index: number;
+//   content: string;
+//   // page: number; // get page of the dom element
+// }
+
+// interface IUser {
+//   id: number;
+//   name: string;
+//   profession: string;
+//   image: string;
+// }
+
+// (page-rendered)="pageRenderedEvent($event)"
+// (text-layer-rendered)="textLayerRenderEvent($event)"
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PdfViewerModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
